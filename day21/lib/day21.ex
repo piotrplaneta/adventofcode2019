0c8c3c37624ec1438fbba23a02c50ddff406f973
piotrplaneta <piotr.planeta@gmail.com>
2019-12-21 10:15:31 +0100

    Add day21 part 2 solution

--- day21/lib/day21.ex
+++ day21/lib/day21.ex
@@ -2,16 +2,35 @@ defmodule Day21 do
   @doc """
   ## Examples
 
+      iex> Day21.hull_damage_with_run_mode()
+      1142686742
+
+  """
+  def hull_damage_with_run_mode do
+    instructions =
+      "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nNOT E T\nAND H T\nOR E T\nAND T J\nRUN\n"
+      |> String.to_charlist()
+
+    execute_computer_with_instructions(instructions)
+  end
+
+  @doc """
+  ## Examples
+
       iex> Day21.hull_damage()
-      221
+      19358262
 
   """
   def hull_damage do
-    instruction =
+    instructions =
       "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n" |> String.to_charlist()
 
+    execute_computer_with_instructions(instructions)
+  end
+
+  defp execute_computer_with_instructions(instructions) do
     Stream.iterate(
-      {:start, instruction, [], init_computer_state(), 0},
+      {:start, instructions, [], init_computer_state(), 0},
       fn {_, inputs, _, state, step} ->
         IntCodeComputer.run(state, step, inputs)
       end
@@ -21,7 +40,7 @@ defmodule Day21 do
     end)
     |> Enum.filter(fn {instruction, _, _, _, _} -> instruction == :output end)
     |> Enum.map(fn {_, _, output, _, _} -> output end)
-    |> IO.puts()
+    |> Enum.at(-1)
   end
 
   defp init_computer_state do
